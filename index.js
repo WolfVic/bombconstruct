0e8a8c49e34011609d5734e6c1d5e18e11575562
Victor Santelé <Vic16@hotmail.be>
2019-01-25 15:30:31 +0100

    update request

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const express = require('express');
+const url = process.env.MONGODB_URI;
 const SocketServer = require('ws').Server;
 const path = require('path');
-
+const MongoClient = require("mongodb").MongoClient;
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
@@ -17,12 +18,27 @@ wss.on('connection', (ws) => {
   console.log('Client connected');
   ws.on('message', (msg) => {
     console.log('received: %s', msg)
-    let res = search(msg)
+    let res = requete(msg).then(exist => exist)
     ws.send(msg)
   })
   ws.on('close', () => console.log('Client disconnected'));
 });
 
-const res = (mot) => {
-
-}
+var requete = async mot => {
+  try {
+    var motExist = -1;
+    var db = await MongoClient.connect(url);
+    console.log("OPEN");
+    var dbo = await db.db("lexique3");
+    var query = { mot: mot };
+    motExist = await dbo
+      .collection("lexique")
+      .find(query)
+      .count();
+    db.close();
+    console.log("motExist : " + motExist);
+    return motExist;
+  } catch (e) {
+    console.log("erreur: ", e);
+  }
+};
